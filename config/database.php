1fd11518598fdac40d9a0f89c6bd83aed90be675
angeloserbo <angeloserbo48@gmail.com>
2026-06-29 15:12:14 +0800

    Add nixpacks configuration and update db connection

--- config/database.php
+++ config/database.php
@@ -1,24 +1,16 @@
 <?php
 
-$host = getenv('MYSQLHOST');
-$database = getenv('MYSQLDATABASE');
-$user = getenv('MYSQLUSER');
-$password = getenv('MYSQLPASSWORD');
-$port = getenv('MYSQLPORT');
+$host = getenv('DB_HOST');
+$db   = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASSWORD');
+$port = getenv('DB_PORT');
 
+$dsn = "mysql:host=$host;dbname=$db;port=$port;charset=utf8mb4";
 try {
-    $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$database;charset=utf8mb4",
-        $user,
-        $password
-    );
-
-    $pdo->setAttribute(
-        PDO::ATTR_ERRMODE,
-        PDO::ERRMODE_EXCEPTION
-    );
-
-} catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+    $pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (\PDOException $e) {
+    exit("Connection failed: " . $e->getMessage());
 }
 ?>
